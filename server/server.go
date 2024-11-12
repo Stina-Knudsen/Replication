eaeae410420c201ff93c8dc7e274a3d5f8e86c4b
AVNBuelow <alby@itu.dk>
2024-11-12 11:37:54 +0100

    general structure is set-up

--- server/server.go
+++ server/server.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"time"
+
+	proto "Replication/grpc"
+)
+
+const auctionDuration = 100 * time.Second
+
+func main() {
+
+}
+
+type AuctionServer struct {
+	proto.UnimplementedAuctionServerServer
+}
